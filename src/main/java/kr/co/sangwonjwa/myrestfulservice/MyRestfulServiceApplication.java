9b990f5f6486a7b3ed41b9295a156e9a9a07d65e
Sangwon Jwa <jwa4610@gmail.com>
2024-01-03 23:02:49 +0900

    feat: 다국어 처리

--- src/main/java/kr/co/sangwonjwa/myrestfulservice/MyRestfulServiceApplication.java
+++ src/main/java/kr/co/sangwonjwa/myrestfulservice/MyRestfulServiceApplication.java
@@ -3,6 +3,11 @@ package kr.co.sangwonjwa.myrestfulservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class MyRestfulServiceApplication {
@@ -11,6 +16,13 @@ public class MyRestfulServiceApplication {
 
         SpringApplication.run(MyRestfulServiceApplication.class, args);
 
+    }
+
+    @Bean
+    public LocaleResolver localeResolver(){
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.US);
+        return localeResolver;
     }
 
 }
--- src/main/java/kr/co/sangwonjwa/myrestfulservice/controller/HelloWorldController.java
+++ src/main/java/kr/co/sangwonjwa/myrestfulservice/controller/HelloWorldController.java
@@ -1,12 +1,21 @@
 package kr.co.sangwonjwa.myrestfulservice.controller;
 
 import kr.co.sangwonjwa.myrestfulservice.bean.HelloWorldBean;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+    private MessageSource messageSource;
+    public HelloWorldController(MessageSource messageSource){
+        this.messageSource = messageSource;
+    }
+
     @GetMapping(path = "/hello-world")
     public String helloWorld(){
         return "Hello World";
@@ -20,5 +29,14 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world-bean/path-variable/{name}")
     public HelloWorldBean helloWorldBeanPathVariable(@PathVariable String name){
         return new HelloWorldBean(String.format("Hello World, %s",name));
+    }
+
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternalized(
+            @RequestHeader(name = "Accept-Language",required = false) Locale locale){
+
+        return messageSource.getMessage("greeting.message",null,locale);
+
+
     }
 }
